631640087e08a03a7e0d8c3bf9ab66513c29f4e4
Suryansh Raj Tripathi <tsuryansh53@gmail.com>
2023-05-24 03:11:01 +0530

    login

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -87,8 +87,8 @@ function Slider() {
 			
 			</Link>
 			<Link to ='/login'>
-			<button className="cta">
-    <span className="hover-underline-animation"> Shop now </span>
+			<button id='login'className="cta">
+    <span className="hover-underline-animation">Login</span>
     <svg viewBox="0 0 46 16" height="10" width="30" xmlns="http://www.w3.org/2000/svg" id="arrow-horizontal">
         <path transform="translate(30)" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" data-name="Path 10" id="Path_10"></path>
     </svg>
