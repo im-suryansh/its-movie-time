6a103311bfb602442643e92001cf4acf8bdebacc
Suryansh Raj Tripathi <tsuryansh53@gmail.com>
2023-05-24 04:10:53 +0530

    Ticket get button

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -9,28 +9,28 @@ function Slider() {
 		{
 			url: "https://images7.alphacoders.com/124/1241136.png",
 			title: "Top Gun Maverick",
-			Trailer: "https://www.youtube.com/watch?v=giXco2jaZ_4",
+			Trailer: "2hr 10mins",
 		},
 		{
 			url: "https://wallpaperaccess.com/full/8652628.jpg",
 			title: "Spiderman: Across The Spiderverse",
-			Trailer: "",
+			Trailer: "2h 16m",
 		},
 		{
 			url: "https://wallpaperaccess.com/full/8999567.jpg",
 			title: "John Wick: Chapter 4",
-			Trailer: "",
+			Trailer: " 2h 49m			",
 		},
 
 		{
 			url: "https://wallpaperaccess.com/full/9048165.jpg",
 			title: "Super Mario Bros: Movie",
-			Trailer: "",
+			Trailer: "1h 32m",
 		},
 		{
 			url: "https://wallpapercave.com/wp/wp11967150.jpg",
 			title: "Fast & The Furious X",
-			Trailer: "",
+			Trailer: " 2h 21m			",
 		},
 	];
 
@@ -60,7 +60,11 @@ function Slider() {
 				id="slider"
 			>
 				<div className="curr">NOW AIRING:</div>
+
 				<div id="title">{slides[currentIndex].title}</div>
+				
+				<div id="time">{slides[currentIndex].Trailer}</div>
+			
 			</div>
 
 			{/* Left Arrow */}
@@ -72,11 +76,18 @@ function Slider() {
 			<div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
 				<BsChevronCompactRight onClick={nextSlide} size={30} />
 			</div>
-		
 			<Link to="/ticket">
 				<a id="buy" className="fancy" href="#">
 					<span className="top-key"></span>
-					<span className="text">Buy Tickets</span>
+					<span className="text">Buy Ticket</span>
+					<span className="bottom-key-1"></span>
+					<span className="bottom-key-2"></span>
+				</a>
+			</Link>
+			<Link to="/Login">
+				<a id="login" className="fancy" href="#">
+					<span className="top-key"></span>
+					<span className="text">LOGIN</span>
 					<span className="bottom-key-1"></span>
 					<span className="bottom-key-2"></span>
 				</a>
